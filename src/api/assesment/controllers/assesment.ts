ca56d86aca7c22c0a063e922a2ba9572d6bf1e08
Ubuntu <ubuntu@ip-172-31-35-1.ap-south-1.compute.internal>
2023-04-08 08:01:29 +0000

    assessments & pathways sync

--- src/api/assesment/controllers/assesment.ts
+++
@@ -1,7 +0,0 @@
-/**
- * assesment controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::assesment.assesment');
--- src/api/assesment/services/assesment.ts
+++
@@ -1,7 +0,0 @@
-/**
- * assesment service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::assesment.assesment');
