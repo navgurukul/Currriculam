b615c686e92cddbf237b0618526c3d928c3e72cf
Ubuntu <ubuntu@ip-172-31-41-69.ap-south-1.compute.internal>
2023-06-19 11:20:14 +0000

    updated schema in prod

--- src/api/offer-letter/controllers/offer-letter.ts
+++
@@ -1,7 +0,0 @@
-/**
- * offer-letter controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::offer-letter.offer-letter');
--- src/api/offer-letter/services/offer-letter.ts
+++
@@ -1,7 +0,0 @@
-/**
- * offer-letter service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::offer-letter.offer-letter');
